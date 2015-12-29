41788579c45fe8d4d55f5d3dc7fd68609ec9a682
johnHackworth <javier.alvarez.lop@gmail.com>
2015-12-29 22:47:10 +0100

    Plugins: Move .org data store to redux

--- client/my-sites/plugins/controller.js
+++ client/my-sites/plugins/controller.js
@@ -69,6 +69,7 @@ function renderSinglePlugin( context, siteUrl, isWpcomPlugin ) {
 			prevPath: lastPluginsListVisited || context.prevPath,
 			prevQuerystring: lastPluginsQuerystring,
 			sites: sites,
+			store: context.store,
 			pluginSlug: pluginSlug,
 			siteUrl: siteUrl,
 			isWpcomPlugin: isWpcomPlugin,
@@ -100,6 +101,7 @@ function renderPluginList( context, basePath, siteUrl ) {
 		React.createElement( PluginListComponent, {
 			path: basePath,
 			context: context,
+			store: context.store,
 			filter: context.params.pluginFilter,
 			sites: sites,
 			search: search
