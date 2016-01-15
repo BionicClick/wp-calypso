73be78e0208e2b0f7ce33d776ef5a0140ac7d43c
Bernhard Reiter <ockham@raz.or.at>
2016-01-15 18:31:48 +0100

    theme-options: Move buttonOptions back inside getButtonOptions()

--- client/my-sites/themes/theme-options.js
+++ client/my-sites/themes/theme-options.js
@@ -13,45 +13,45 @@ import partial from 'lodash/function/partial';
 import Helper from 'lib/themes/helpers';
 import actionLabels from './action-labels';
 
-const buttonOptions = {
-	signup: {
-		hasUrl: true,
-		hideForSite: ( site, isLoggedOut ) => ! isLoggedOut
-	},
-	preview: {
-		hasAction: true,
-		hasUrl: false,
-		hideForTheme: theme => theme.active
-	},
-	purchase: {
-		hasAction: true,
-		hideForSite: ( site, isLoggedOut ) => isLoggedOut,
-		hideForTheme: theme => theme.active || theme.purchased || ! theme.price
-	},
-	activate: {
-		hasAction: true,
-		hideForSite: ( site, isLoggedOut ) => isLoggedOut,
-		hideForTheme: theme => theme.active || ( theme.price && ! theme.purchased )
-	},
-	customize: {
-		hasAction: true,
-		hideForSite: ( site, isLoggedOut ) => isLoggedOut && ( site && ! site.isCustomizable() ),
-		hideForTheme: theme => ! theme.active
-	},
-	separator: {
-		separator: true
-	},
-	details: {
-		hasUrl: true
-	},
-	support: {
-		hasUrl: true,
-		hideForSite: site => site && site.jetpack // We don't know where support docs for a given theme on a self-hosted WP install are.
-	},
-};
-
 export function getButtonOptions( site, isLoggedOut, actions, setSelectedTheme, togglePreview ) {
-	let options = pick( buttonOptions, option => ! ( option.hideForSite && option.hideForSite( site, isLoggedOut ) ) );
+	const buttonOptions = {
+		signup: {
+			hasUrl: true,
+			hideForSite: ! isLoggedOut
+		},
+		preview: {
+			hasAction: true,
+			hasUrl: false,
+			hideForTheme: theme => theme.active
+		},
+		purchase: {
+			hasAction: true,
+			hideForSite: isLoggedOut,
+			hideForTheme: theme => theme.active || theme.purchased || ! theme.price
+		},
+		activate: {
+			hasAction: true,
+			hideForSite: isLoggedOut,
+			hideForTheme: theme => theme.active || ( theme.price && ! theme.purchased )
+		},
+		customize: {
+			hasAction: true,
+			hideForSite: isLoggedOut && ( site && ! site.isCustomizable() ),
+			hideForTheme: theme => ! theme.active
+		},
+		separator: {
+			separator: true
+		},
+		details: {
+			hasUrl: true
+		},
+		support: {
+			hasUrl: true,
+			hideForSite: site && site.jetpack // We don't know where support docs for a given theme on a self-hosted WP install are.
+		},
+	};
+
+	let options = pick( buttonOptions, option => ! option.hideForSite );
 	options = mapValues( options, appendLabelAndHeader );
 	options = mapValues( options, appendUrl );
 	options = mapValues( options, appendAction );
