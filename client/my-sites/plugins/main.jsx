a6c8f1cc4609b7b1ceaab4dfd485af4abb46aebc
Enej Bajgoric <enej.bajgoric@gmail.com>
2016-01-15 15:42:31 -0800

    Plugins: Update PluginList to PluginsMain

--- client/my-sites/plugins/main.jsx
+++ client/my-sites/plugins/main.jsx
@@ -45,9 +45,9 @@ import PlanNudge from 'components/plans/plan-nudge';
  */
 const debug = debugModule( 'calypso:my-sites:plugins' );
 
-const PluginsList = React.createClass( {
+const PluginsMain = React.createClass( {
 
-	displayName: 'PluginsList',
+	displayName: 'PluginsMain',
 
 	mixins: [ URLSearch ],
 
@@ -352,6 +352,7 @@ const PluginsList = React.createClass( {
 	},
 
 	render() {
+		console.log( 'hello' );
 		if ( this.state.accessError ) {
 			return (
 				<Main>
@@ -440,4 +441,4 @@ export default connect(
 		};
 	},
 	dispatch => bindActionCreators( { fetchPluginData }, dispatch )
-)( PluginsList );
+)( PluginsMain );
